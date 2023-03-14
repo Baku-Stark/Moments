433f4270f3cdb9783301f96d5c743fa12e6aeaaf
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-03-14 18:32:29 -0300

    🪁 Update: Projeto em Angular JS

--- moments/AppMoments/src/app/components/moment-form/moment-form.component.ts
+++ moments/AppMoments/src/app/components/moment-form/moment-form.component.ts
@@ -13,6 +13,7 @@ import { Moment } from 'src/app/Moments';
 export class MomentFormComponent {
   @Output() onSubmit = new EventEmitter<Moment>()
   @Input() btnText!: string
+  @Input() momentData: Moment | null = null
 
 
   momentForm!: FormGroup
--- moments/AppMoments/src/app/components/pages/edit/edit.component.ts
+++ moments/AppMoments/src/app/components/pages/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// IMPORT [interface]
+import { Moment } from 'src/app/Moments';
+import { MomentsService } from 'src/app/services/moments.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent {
+  moment!: Moment
+
+  btnText:string = 'Editar'
+
+  constructor(
+    private momenService: MomentsService,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit():void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+
+    this.momenService.getMomentID(id).subscribe((item) => {
+      this.moment = item.data
+    })
+  }
 
 }
